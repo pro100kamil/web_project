dd32ffd86ff4070901050e2a994169bd49eb2ab8
Emil <askarov.emii@yandex.ru>
2021-04-17 17:37:25 +0500

    Добавлены модели поста и темы поста

--- data/__all_models.py
+++ data/__all_models.py
@@ -1,1 +1,3 @@
 from . import users
+from . import categories
+from . import posts
---
+++ data/posts.py
@@ -0,0 +1,23 @@
+import sqlalchemy
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+import datetime
+from sqlalchemy_serializer import SerializerMixin
+
+
+class Post(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'posts'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    author_id = sqlalchemy.Column(sqlalchemy.Integer,
+                                  sqlalchemy.ForeignKey('users.id'))
+    title = sqlalchemy.Column(sqlalchemy.String, default='Безымяный')
+    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
+
+    categories = orm.relation("Category",
+                              secondary="association",
+                              backref="posts")
+
