e5473c6e9fa9b438bf42c5c8c1d25caa863128bb
выауу <camgal04@gmail.com>
2021-04-19 21:53:26 +0500

    отображение подписчиков и нормальное отображение многострочных постов

--- data/users.py
+++ data/users.py
@@ -7,11 +7,25 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from .db_session import SqlAlchemyBase
 
 followers = sqlalchemy.Table('followers', SqlAlchemyBase.metadata,
-                             sqlalchemy.Column('follower_id', sqlalchemy.Integer,
-                                               sqlalchemy.ForeignKey('users.id')),
-                             sqlalchemy.Column('followed_id', sqlalchemy.Integer,
-                                               sqlalchemy.ForeignKey('users.id'))
-)
+                             sqlalchemy.Column('follower_id',
+                                               sqlalchemy.Integer,
+                                               sqlalchemy.ForeignKey(
+                                                   'users.id')),
+                             sqlalchemy.Column('followed_id',
+                                               sqlalchemy.Integer,
+                                               sqlalchemy.ForeignKey(
+                                                   'users.id'))
+                             )
+subscribers = sqlalchemy.Table('subscribers', SqlAlchemyBase.metadata,
+                               sqlalchemy.Column('subscriber_id',
+                                                 sqlalchemy.Integer,
+                                                 sqlalchemy.ForeignKey(
+                                                     'users.id')),
+                               sqlalchemy.Column('subscribed_id',
+                                                 sqlalchemy.Integer,
+                                                 sqlalchemy.ForeignKey(
+                                                     'users.id'))
+                               )
 
 
 class User(SqlAlchemyBase, UserMixin, SerializerMixin):
@@ -33,17 +47,29 @@ class User(SqlAlchemyBase, UserMixin, SerializerMixin):
                                 secondary=followers,
                                 primaryjoin=(followers.c.follower_id == id),
                                 secondaryjoin=(followers.c.followed_id == id),
-                                backref=orm.backref('followers', lazy='dynamic'),
+                                backref=orm.backref('followers',
+                                                    lazy='dynamic'),
                                 lazy='dynamic')
+    subscribed = orm.relationship('User',
+                                  secondary=subscribers,
+                                  primaryjoin=(
+                                          subscribers.c.subscriber_id == id),
+                                  secondaryjoin=(
+                                          subscribers.c.subscribed_id == id),
+                                  backref=orm.backref('subscribers',
+                                                      lazy='dynamic'),
+                                  lazy='dynamic')
 
     def follow(self, user):
         if user.id != self.id and not self.is_following(user):
             self.followed.append(user)
+            user.subscribed.append(self)
             return self
 
     def unfollow(self, user):
         if self.is_following(user):
             self.followed.remove(user)
+            user.subscribed.remove(self)
             return self
 
     def is_following(self, user):
