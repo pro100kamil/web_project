88331510c86d2ec8ca4ad8885906b0b452502459
выауу <camgal04@gmail.com>
2021-04-25 15:42:28 +0500

    подготовка к deploy

--- main.py
+++ main.py
@@ -575,6 +575,8 @@ if __name__ == '__main__':
     app.register_blueprint(users_api.blueprint)
     app.register_blueprint(posts_api.blueprint)
 
-    # port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+
+    # app.run(host='127.0.0.1', port=5000)
     # app.run(debug=True)
