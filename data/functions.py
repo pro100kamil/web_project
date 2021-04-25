674718b78e76f987ef9e607769dbe6b5918fb614
Emil <askarov.emii@yandex.ru>
2021-04-25 13:26:27 +0500

    Дизайн отображения одного поста

--- data/functions.py
+++ data/functions.py
@@ -3,23 +3,18 @@ from .comments import Comment
 from flask import url_for
 
 HTML_COMMENTS = '''<div class="mar-btm">
- <a href="#" class="btn-link text-semibold media-heading box-inline">{username}</a>
+ <a href="/users/{user_id}" class="btn-link text-semibold media-heading box-inline">{username}</a>
  <p class="text-muted text-sm"><i class="fa fa-mobile fa-lg"></i>{date}</p>
  </div>
  <p>{text}</p>
  <div class="pad-ver">
- <div class="btn-group">
- <a class="btn btn-sm btn-default btn-hover-success" href="#"><i class="fa fa-thumbs-up"></i></a>
- <a class="btn btn-sm btn-default btn-hover-danger" href="#"><i class="fa fa-thumbs-down"></i></a>
- </div>
- <a class="btn btn-sm btn-default btn-hover-primary" href="{url}">Ответить</a>
+ <a class="btn btn-sm btn-default btn-hover-primary btn-outline-info" href="{url}">Ответить</a>
  </div>
  <hr>'''
 
-HTML_FORM = '''   <form action="" method="post" class="form-inline pb-2">
+HTML_FORM = '''   <form action="" method="post" class="form-inline pb-2" style="padding-left: 5px;">
         {{ form.hidden_tag() }}
         <p>
-            {{ form.content.label }}<br>
             {{ form.content(class="form-control") }}
             {% for error in form.content.errors %}
                 <p class="alert alert-danger" role="alert">
@@ -44,14 +39,16 @@ def reformat_comments(comments: List[Comment], to_id=None, reply_id=None):
                                       text=comment.text,
                                       url=url_for('show_post',
                                                   post_id=comment.post_id,
-                                                  reply_to=comment.id))
+                                                  reply_to=comment.id), user_id=comment.user_id)
         text_2 = '''<div class="media-block">
-                        <a class="media-left" href="#">
+                        <a class="media-left" href="/users/{user_id}">
                             <img class="img-circle img-sm" 
-                            alt="Профиль пользователя" src="{url}">
+                            alt="Профиль пользователя" src="{url}" style="border-radius: 50%;
+                            margin-right: 20px;">
                         </a>
                     <div class="media-body">'''.format(
-            url=url_for('static', filename='img/users/') + comment.user.icon)
+            url=url_for('static', filename='img/users/') + comment.user.icon,
+            user_id=comment.user_id)
         html_code += text_2 + text_1 + reformat_comments(comments, comment.id,
                                                          reply_id)
 
