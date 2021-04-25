8c071c04a1506c9aed9965a9b43be9221ff203f2
Emil <askarov.emii@yandex.ru>
2021-04-25 12:51:31 +0500

    Изменил отображение постов на главной странице

--- main.py
+++ main.py
@@ -555,7 +555,7 @@ def search():
         elif kind == 'posts':
             posts = db_sess.query(Post).filter(
                 Post.title.like(f'%{line}%')).all()
-            posts = posts[(page - 1) * max_results:page * max_results]
+            posts = posts[::-1][(page - 1) * max_results:page * max_results]
             return render_template('search_posts.html', posts=posts,
                                    title='Поиск поста: ' + line, **params)
 
@@ -575,6 +575,6 @@ if __name__ == '__main__':
     app.register_blueprint(users_api.blueprint)
     app.register_blueprint(posts_api.blueprint)
 
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    # port = int(os.environ.get("PORT", 5000))
+    app.run(host='127.0.0.1', port=5000)
     # app.run(debug=True)
