16318a9b7127980759f062ce89c8e0f2b4cbba97
выауу <camgal04@gmail.com>
2021-04-25 18:16:17 +0500

    изменил хост

--- data/config.py
+++ data/config.py
@@ -4,3 +4,4 @@ VK_PASSWORD = ''
 TG_TOKEN = '1773431202:AAEQ-JoWlUCJ1OjR8kFBWd44PJWUSdYQ_Fk'
 NEWS_API_KEY = '5f80be67653d478e82094179730f4798'
 SITE = 'http://blog1kamil1emil.herokuapp.com'
+# SITE = 'http://http://127.0.0.1:5000/'  # локальный запуск
--- main.py
+++ main.py
@@ -585,7 +585,7 @@ if __name__ == '__main__':
     app.register_blueprint(users_api.blueprint)
     app.register_blueprint(posts_api.blueprint)
 
-    # port = int(os.environ.get("PORT", 5000))
-    # app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
-    app.run(host='127.0.0.1', port=5000)
+    # app.run(host='127.0.0.1', port=5000)  # локальный запуск
