0c6ecd3966e5f62409d8e3b4c89da93bc25ba700
Emil <74973350+EmiAsk@users.noreply.github.com>
2021-05-28 17:39:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -597,6 +597,6 @@ if __name__ == '__main__':
     app.register_blueprint(users_api.blueprint)
     app.register_blueprint(posts_api.blueprint)
 
-
-
-    app.run(host='127.0.0.1', port=5000)  # локальный запуск
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
+    # app.run(host='127.0.0.1', port=5000)  # локальный запуск
